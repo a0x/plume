19f5a176678d9f37df0138660a5e3000488f07c4
Alvin Qi <peng.edy@gmail.com>
2016-03-01 21:59:07 +0800

    add annotation and questions in plume.js

--- www/public/plume.js
+++ www/public/plume.js
@@ -1,5 +1,33 @@
+/*
+ * In this file of code, the author did three things:
+ *   1. init the editor
+ *   2. init the doc typed rich-text using sharejs
+ *   3. bound `cursor` onto the editor, and communicate with the doc:
+ *     3.1 data sync
+ *     3.2 cursor sync
+ *
+ * However, there is at least one thing to be done:
+ *   to deal with the lock in `doc`.
+ *
+ */
+
+
+// `userId` is the key to different users.
+var userId = 'id-' + Math.random(10) * 1000;
+var editor = new Quill('#editor', {
+    modules: {
+        'toolbar'     : {container: '#toolbar'},
+        'authorship'  : {authorId: userId, enabled: true, color:  'rgb(255, 0, 255)', button: $('#authors')[0]},
+        'link-tooltip': true
+    },
+    theme  : 'snow'
+});
+console.log(editor);
+
+// Init ShareJS
 var socket = new BCSocket(null, {reconnect: true});
 var sjs = new sharejs.Connection(socket);
+// Register the ot-type as rich-text
 sharejs.registerType(window.ottypes['rich-text']);
 
 var doc = sjs.get('docs', 'hello2');
@@ -10,29 +38,17 @@ doc.subscribe();
 // This will be called when we have a live copy of the server's data.
 doc.whenReady(function () {
 
-	var userId = 'id-' + Math.random(10) * 1000;
-
 	console.log('doc ready, data: ', doc);
-
 
 	// Create a rich-text doc
 	if (!doc.type) {
 		console.log('doc has not type - trying to create a rich-text doc');
+        // This line of code does the thing that created a doc as rich-text, and set default as ''.
 		doc.create('rich-text', '');
 		console.log('created as ', doc);
 	}
 
-	var editor = new Quill('#editor', {
-		modules: {
-			'toolbar'     : {container: '#toolbar'},
-			'authorship'  : {authorId: userId, enabled: true, color:  'rgb(255, 0, 255)', button: $('#authors')[0]},
-			'link-tooltip': true
-		},
-		theme  : 'snow'
-	});
-
 	//editor.modules.authorship.attachButton($('#authors'));
-	console.log(editor);
 	var multiCursor = editor.addModule('multi-cursor', {
 		timeout: 10000
 	});
@@ -41,12 +57,21 @@ doc.whenReady(function () {
 	editor.updateContents(doc.getSnapshot());
 
 	// Cursor handling managed by Primus which is a socket wrapper
-	var primus = Primus.connect('', {});
 
+    // init Primus
+	var primus = Primus.connect('', {});
+    window.primus = primus;
+
+    /* 1st event
+     * TODO: what does 'open' mean?
+     */
 	primus.on('open', function open() {
 		console.log('Connection is alive and kicking');
 	});
 
+    /* 2nd event: sync the cursors when data comes in. */
+    // Consider `data` as users' input. And this event is the most
+    // important one of all the events bound to `primus`
 	primus.on('data', function message(data) {
 		var cursor =  data.cursor;
 		multiCursor.removeCursor(cursor.id);
@@ -54,12 +79,14 @@ doc.whenReady(function () {
 		console.log('Received a new message from the server: ', data.cursor);
 	});
 
+    /* 3rd event: error alert. */
 	primus.on('error', function error(err) {
 		console.error('Something horrible has happened', err.stack);
 	});
 
 	//************ end ***************//
 
+    /* 4th event: dealing with text selection. */
 	editor.on('selection-change', function (range) {
 		var c = hexToRgb($('#cursor-colour').val());
 		var colour = 'rgb(' + c.r + ', ' + c.g + ', ' + c.b + ')';
@@ -68,16 +95,18 @@ doc.whenReady(function () {
 			'colour': colour,
 			'name': $('#name').val(),
 			'id': userId
-		}
+		};
 
 		primus.write({'cursor': cursor});
 	});
 
+    /* 5th event: submit local text change as rich-text ot. */
 	editor.on('text-change', function (delta, source) {
-		console.log('text-change', delta, source)
+		console.log('text-change', delta, source);
 		doc.submitOp(delta);
 	});
 
+    /* 6th event: update local content with rich-text ot from remote. */
 	doc.on('op', function (op, localContext) {
 		if (!localContext) {
 			editor.updateContents(op.ops);
@@ -85,6 +114,7 @@ doc.whenReady(function () {
 	})
 });
 
+// This function is used to mark the multiple cursors.
 function hexToRgb(hex) {
 	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 	return result ? {
@@ -93,3 +123,19 @@ function hexToRgb(hex) {
 		b: parseInt(result[3], 16)
 	} : null;
 }
+
+
+/*
+ * Questions:
+ * 0. What is 'multi-cursor' module?
+ * 1. What is Primus?
+ * 2. Why need multi-cursor?
+ * 3. Did the author customize the Quill Editor?
+ * 4. How does Simditor integrate Primus?
+ * 5. How does Quill editor deal with ot formatted in rich-text?
+ * 6. How should rich-text ot be applied?
+ * 7. How does Simditor apply ot formatted in rich-text?
+ * 8. What's the error/bug inside this project?
+ * 9. Can the order of these 6 events changed?
+ * 10. How does Quill editor `updateContents()`?
+ */
